2fab2a029ef786829d6be750d14952ccaa8e444b
Rahul Aggarwal <rahaggar@adobe.com>
2025-02-07 15:36:50 +0530

    linitng

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -10,25 +10,23 @@ function buildCell(rowIndex) {
     return cell;
   }
   
-  export default async function decorate(block) {
-    const table = document.createElement('table');
-    const thead = document.createElement('thead');
-    const tbody = document.createElement('tbody');
-  
-    const header = !block.classList.contains('no-header');
-    if (header) table.append(thead);
-    table.append(tbody);
-  
-    [...block.children].forEach((child, i) => {
-      const row = document.createElement('tr');
-      if (header && i === 0) thead.append(row);
-      else tbody.append(row);
-      [...child.children].forEach((col) => {
-        const cell = buildCell(header ? i : i + 1);
-        cell.innerHTML = col.innerHTML;
-        row.append(cell);
-      });
+export default async function decorate(block) {
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+  const header = !block.classList.contains('no-header');
+  if (header) table.append(thead);
+  table.append(tbody);
+
+  [...block.children].forEach((child, i) => {
+    const row = document.createElement('tr');
+    if (header && i === 0) thead.append(row);
+    else tbody.append(row);
+    [...child.children].forEach((col) => {
+      const cell = buildCell(header ? i : i + 1);
+      cell.innerHTML = col.innerHTML;
+      row.append(cell);
     });
-    block.innerHTML = '';
-    block.append(table);
-  }
+  });
+}
