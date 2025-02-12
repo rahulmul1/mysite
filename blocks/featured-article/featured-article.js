25d7d5c37da2bedd9d8167db8de2ccf6d684c0a4
Rahul Aggarwal <rahaggar@adobe.com>
2025-02-12 12:31:47 +0530

    fix

--- blocks/featured-article/featured-article.js
+++ blocks/featured-article/featured-article.js
@@ -1,17 +1,17 @@
 export default function decorateFeaturedArticle(block) {
-  // Step 2: Get the direct children inside the block
-  const children = [...block.children];
-  // Step 3: Add classes to the content and image divs
-  children.forEach((child) => {
-    const innerChildren = [...child.children];
-    innerChildren.forEach((innerChild) => {
-      const pictureElement = innerChild.querySelector('picture');
-      if (pictureElement) {
-        innerChild.classList.add('featured-article-image');
-        pictureElement.classList.add('featured-article-picture');
-      } else {
-        innerChild.classList.add('featured-article-content');
-      }
+    // Step 2: Get the direct children inside the block
+    const children = [...block.children];
+    // Step 3: Add classes to the content and image divs
+    children.forEach((child) => {
+        const innerChildren = [...child.children];
+        innerChildren.forEach((innerChild) => {
+            const pictureElement = innerChild.querySelector('picture');
+            if (pictureElement) {
+                innerChild.classList.add('featured-article-image');
+                pictureElement.classList.add('featured-article-picture');
+            } else {
+                innerChild.classList.add('featured-article-content');
+            }
+        });
     });
-  });
 }
