cb9a9fff52102acde4b5ec0853d512ff33a89048
Rahul Aggarwal <rahaggar@adobe.com>
2025-02-13 12:54:54 +0530

    wip

--- blocks/recent-articles/recent-articles.js
+++ blocks/recent-articles/recent-articles.js
@@ -1,5 +1,4 @@
 export default function decorateRecentArticles(block) {
-
   // Step 2: Get the children inside the block
   const children = [...block.children];
 
@@ -12,17 +11,20 @@ export default function decorateRecentArticles(block) {
       // Step 4: Add classes to the picture and content elements
       const pictureElement = articleDiv.querySelector('picture');
       if (pictureElement) {
-        pictureElement.classList.add('recent-article-picture');
+        const pictureParent = pictureElement.closest('p');
+        if (pictureParent) {
+          pictureParent.classList.add('recent-article-picture');
+        }
       }
 
-      const buttonContainer = articleDiv.querySelector('.button-container');
-      if (buttonContainer) {
-        buttonContainer.classList.add('recent-article-button-container');
+      const linkContainer = articleDiv.querySelector('h4');
+      if (linkContainer) {
+        linkContainer.classList.add('recent-article-link-container');
       }
 
       const paragraphs = articleDiv.querySelectorAll('p');
       paragraphs.forEach((paragraph) => {
-        if (!paragraph.querySelector('picture')) {
+        if (!paragraph.querySelector('picture') && !paragraph.querySelector('a')) {
           paragraph.classList.add('recent-article-content');
         }
       });
